d1c69b9781c603f4b317b1d8df3d939cb3e5e99e
katarighe <m.ighe@hotmail.com>
2023-09-28 19:06:02 +0300

    Move this to tests file

--- my_list.rb
+++ my_list.rb
@@ -11,19 +11,3 @@ class MyList
     @list.each(&block)
   end
 end
-
-# TESTS
-all?
-list = MyList.new(1, 2, 3, 4)
-puts(list.all? { |e| e < 5 })
-puts(list.all? { |e| e > 5 })
-
-any?
-puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
-
-filter
-list = MyList.new(1, 2, 3, 4, 5)
-filtered_list = list.filter(&:even?)
-puts filtered_list.inspect
-list.each { |e| print "#{e} " }
